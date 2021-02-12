12dd9b416c8041fadfe0e999b6d4e047dddc1ff1
Maria Jose Bernal <71986858+mjbernalv@users.noreply.github.com>
2021-02-12 10:24:54 -0500

    Delete Permutations.py

--- talleres/taller03/Permutations.py
+++
@@ -1,8 +0,0 @@
-def permutations(base, stri):
-    if len(stri)==0:
-        print(base) 
-    else:
-        for i in range (len(stri)):
-            permutations(base+stri[i], stri[0:i] + stri[i+1:])
-
-permutations("", "abc")
